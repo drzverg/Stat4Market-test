15cbead50f950d8bc810a4eccdf7bc85c5c9d75a
Dr. Zverg <zvergus@gmail.com>
2022-05-03 17:20:29 +0300

    decoration completed

--- docs/default.js
+++ docs/default.js
@@ -1,99 +1,11 @@
-/*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
-/******/ 	var __webpack_modules__ = ({
+var __webpack_exports__ = {};
+//Default styles import
 
-/***/ "./src/blocks/header/header.scss":
-/*!***************************************!*\
-  !*** ./src/blocks/header/header.scss ***!
-  \***************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://Trevland/./src/blocks/header/header.scss?");
+//Blocks import
 
-/***/ }),
 
-/***/ "./src/layouts/default/default.scss":
-/*!******************************************!*\
-  !*** ./src/layouts/default/default.scss ***!
-  \******************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://Trevland/./src/layouts/default/default.scss?");
-
-/***/ }),
-
-/***/ "./src/blocks/header/header.js":
-/*!*************************************!*\
-  !*** ./src/blocks/header/header.js ***!
-  \*************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _header_header_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../header/header.scss */ \"./src/blocks/header/header.scss\");\n\r\n\n\n//# sourceURL=webpack://Trevland/./src/blocks/header/header.js?");
-
-/***/ }),
-
-/***/ "./src/layouts/default/default.js":
-/*!****************************************!*\
-  !*** ./src/layouts/default/default.js ***!
-  \****************************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _default_default_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../default/default.scss */ \"./src/layouts/default/default.scss\");\n/* harmony import */ var _blocks_header_header__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../../blocks/header/header */ \"./src/blocks/header/header.js\");\n//Default styles import\r\n\r\n\r\n//Blocks import\r\n\r\n\n\n//# sourceURL=webpack://Trevland/./src/layouts/default/default.js?");
-
-/***/ })
-
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/layouts/default/default.js");
-/******/ 	
 /******/ })()
 ;
--- docs/index.js
+++ docs/index.js
@@ -1,132 +1,74 @@
-/*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
 /******/ (() => { // webpackBootstrap
 /******/ 	"use strict";
 /******/ 	var __webpack_modules__ = ({
 
-/***/ "./src/styles.scss":
-/*!*************************!*\
-  !*** ./src/styles.scss ***!
-  \*************************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://Trevland/./src/styles.scss?");
-
-/***/ }),
-
-/***/ "./src/index.js":
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
-
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _src_styles_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../src/styles.scss */ \"./src/styles.scss\");\n/* harmony import */ var _src_assets_img_story_1_webp__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ../src/assets/img/story_1.webp */ \"./src/assets/img/story_1.webp\");\n/* harmony import */ var _src_assets_img_story_2_webp__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../src/assets/img/story_2.webp */ \"./src/assets/img/story_2.webp\");\n/* harmony import */ var _src_assets_img_stat4market_svg__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ../src/assets/img/stat4market.svg */ \"./src/assets/img/stat4market.svg\");\n/* harmony import */ var _src_assets_img_mask_svg__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ../src/assets/img/mask.svg */ \"./src/assets/img/mask.svg\");\n/* harmony import */ var _src_assets_img_icons_arrow_down_svg__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ../src/assets/img/icons/arrow_down.svg */ \"./src/assets/img/icons/arrow_down.svg\");\n/* harmony import */ var _src_assets_img_icons_arrow_right_svg__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(/*! ../src/assets/img/icons/arrow_right.svg */ \"./src/assets/img/icons/arrow_right.svg\");\n/* harmony import */ var _src_assets_img_icons_carbon_phone_filled_svg__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(/*! ../src/assets/img/icons/carbon_phone-filled.svg */ \"./src/assets/img/icons/carbon_phone-filled.svg\");\n/* harmony import */ var _src_assets_img_icons_sprite_svg__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(/*! ../src/assets/img/icons/sprite.svg */ \"./src/assets/img/icons/sprite.svg\");\n/* harmony import */ var _src_assets_img_Bg_stars_webp__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(/*! ../src/assets/img/Bg stars.webp */ \"./src/assets/img/Bg stars.webp\");\n/* harmony import */ var _src_assets_img_Superman_webp__WEBPACK_IMPORTED_MODULE_10__ = __webpack_require__(/*! ../src/assets/img/Superman.webp */ \"./src/assets/img/Superman.webp\");\n// Import Styles =========================\r\n\r\n\r\n// Import images =========================\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\r\n\n\n//# sourceURL=webpack://Trevland/./src/index.js?");
-
-/***/ }),
-
-/***/ "./src/assets/img/Bg stars.webp":
-/*!**************************************!*\
-  !*** ./src/assets/img/Bg stars.webp ***!
-  \**************************************/
+/***/ 526:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("module.exports = __webpack_require__.p + \"assets/img/Bg stars.webp\";\n\n//# sourceURL=webpack://Trevland/./src/assets/img/Bg_stars.webp?");
+module.exports = __webpack_require__.p + "assets/img/Bg stars.webp";
 
 /***/ }),
 
-/***/ "./src/assets/img/Superman.webp":
-/*!**************************************!*\
-  !*** ./src/assets/img/Superman.webp ***!
-  \**************************************/
+/***/ 833:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("module.exports = __webpack_require__.p + \"assets/img/Superman.webp\";\n\n//# sourceURL=webpack://Trevland/./src/assets/img/Superman.webp?");
+module.exports = __webpack_require__.p + "assets/img/Superman.webp";
 
 /***/ }),
 
-/***/ "./src/assets/img/icons/arrow_down.svg":
-/*!*********************************************!*\
-  !*** ./src/assets/img/icons/arrow_down.svg ***!
-  \*********************************************/
+/***/ 257:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("module.exports = __webpack_require__.p + \"assets/img/icons/arrow_down.svg\";\n\n//# sourceURL=webpack://Trevland/./src/assets/img/icons/arrow_down.svg?");
+module.exports = __webpack_require__.p + "assets/img/icons/arrow_down.svg";
 
 /***/ }),
 
-/***/ "./src/assets/img/icons/arrow_right.svg":
-/*!**********************************************!*\
-  !*** ./src/assets/img/icons/arrow_right.svg ***!
-  \**********************************************/
+/***/ 76:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("module.exports = __webpack_require__.p + \"assets/img/icons/arrow_right.svg\";\n\n//# sourceURL=webpack://Trevland/./src/assets/img/icons/arrow_right.svg?");
+module.exports = __webpack_require__.p + "assets/img/icons/arrow_right.svg";
 
 /***/ }),
 
-/***/ "./src/assets/img/icons/carbon_phone-filled.svg":
-/*!******************************************************!*\
-  !*** ./src/assets/img/icons/carbon_phone-filled.svg ***!
-  \******************************************************/
+/***/ 404:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("module.exports = __webpack_require__.p + \"assets/img/icons/carbon_phone-filled.svg\";\n\n//# sourceURL=webpack://Trevland/./src/assets/img/icons/carbon_phone-filled.svg?");
+module.exports = __webpack_require__.p + "assets/img/icons/carbon_phone-filled.svg";
 
 /***/ }),
 
-/***/ "./src/assets/img/icons/sprite.svg":
-/*!*****************************************!*\
-  !*** ./src/assets/img/icons/sprite.svg ***!
-  \*****************************************/
+/***/ 180:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("module.exports = __webpack_require__.p + \"assets/img/icons/sprite.svg\";\n\n//# sourceURL=webpack://Trevland/./src/assets/img/icons/sprite.svg?");
+module.exports = __webpack_require__.p + "assets/img/icons/sprite.svg";
 
 /***/ }),
 
-/***/ "./src/assets/img/mask.svg":
-/*!*********************************!*\
-  !*** ./src/assets/img/mask.svg ***!
-  \*********************************/
+/***/ 105:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("module.exports = __webpack_require__.p + \"assets/img/mask.svg\";\n\n//# sourceURL=webpack://Trevland/./src/assets/img/mask.svg?");
+module.exports = __webpack_require__.p + "assets/img/mask.svg";
 
 /***/ }),
 
-/***/ "./src/assets/img/stat4market.svg":
-/*!****************************************!*\
-  !*** ./src/assets/img/stat4market.svg ***!
-  \****************************************/
+/***/ 525:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("module.exports = __webpack_require__.p + \"assets/img/stat4market.svg\";\n\n//# sourceURL=webpack://Trevland/./src/assets/img/stat4market.svg?");
+module.exports = __webpack_require__.p + "assets/img/stat4market.svg";
 
 /***/ }),
 
-/***/ "./src/assets/img/story_1.webp":
-/*!*************************************!*\
-  !*** ./src/assets/img/story_1.webp ***!
-  \*************************************/
+/***/ 418:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("module.exports = __webpack_require__.p + \"assets/img/story_1.webp\";\n\n//# sourceURL=webpack://Trevland/./src/assets/img/story_1.webp?");
+module.exports = __webpack_require__.p + "assets/img/story_1.webp";
 
 /***/ }),
 
-/***/ "./src/assets/img/story_2.webp":
-/*!*************************************!*\
-  !*** ./src/assets/img/story_2.webp ***!
-  \*************************************/
+/***/ 330:
 /***/ ((module, __unused_webpack_exports, __webpack_require__) => {
 
-eval("module.exports = __webpack_require__.p + \"assets/img/story_2.webp\";\n\n//# sourceURL=webpack://Trevland/./src/assets/img/story_2.webp?");
+module.exports = __webpack_require__.p + "assets/img/story_2.webp";
 
 /***/ })
 
@@ -169,17 +111,6 @@ eval("module.exports = __webpack_require__.p + \"assets/img/story_2.webp\";\n\n/
 /******/ 		})();
 /******/ 	})();
 /******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/publicPath */
 /******/ 	(() => {
 /******/ 		var scriptUrl;
@@ -201,11 +132,35 @@ eval("module.exports = __webpack_require__.p + \"assets/img/story_2.webp\";\n\n/
 /******/ 	})();
 /******/ 	
 /************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
-/******/ 	
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/* harmony import */ var _src_assets_img_story_1_webp__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(418);
+/* harmony import */ var _src_assets_img_story_2_webp__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(330);
+/* harmony import */ var _src_assets_img_stat4market_svg__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(525);
+/* harmony import */ var _src_assets_img_mask_svg__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(105);
+/* harmony import */ var _src_assets_img_icons_arrow_down_svg__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(257);
+/* harmony import */ var _src_assets_img_icons_arrow_right_svg__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(76);
+/* harmony import */ var _src_assets_img_icons_carbon_phone_filled_svg__WEBPACK_IMPORTED_MODULE_6__ = __webpack_require__(404);
+/* harmony import */ var _src_assets_img_icons_sprite_svg__WEBPACK_IMPORTED_MODULE_7__ = __webpack_require__(180);
+/* harmony import */ var _src_assets_img_Bg_stars_webp__WEBPACK_IMPORTED_MODULE_8__ = __webpack_require__(526);
+/* harmony import */ var _src_assets_img_Superman_webp__WEBPACK_IMPORTED_MODULE_9__ = __webpack_require__(833);
+// Import Styles =========================
+
+
+// Import images =========================
+
+
+
+
+
+
+
+
+
+
+
+})();
+
 /******/ })()
 ;
